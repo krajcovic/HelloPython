9ebfdbec07911d19ac03481b4266b61ac5b16fd0
Krajcovic Dusan <dusan.krajcovic@gmail.com>
2015-06-09 21:25:25 +0200

    Merged level6

--- levels/level6.py
+++ levels/level6.py
@@ -5,9 +5,13 @@ __author__ = 'krajcovic'
 __name__ = 'level6'
 
 import fibo
+import sys
 #import py_compile
 
 #py_compile.compile(__name__ + '.py')
+
+
+print(sys.path)
 
 fibo.fib(100)
 print(fibo.fib2(100))
