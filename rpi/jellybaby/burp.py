12bc282c38b88147af7147c085ca3614e9fa5441
Dusan <dusan.krajcovic@gmail.com>
2015-06-22 23:22:28 +0200

    jellybaby on port 4.

--- rpi/jellybaby/burp.py
+++ rpi/jellybaby/burp.py
@@ -6,6 +6,18 @@ __author__ = 'krajcovic'
 import time
 import RPi.GPIO as GPIO
 import os
+import threading
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(4, GPIO.IN)
+print("Press control button")
+while True:
+    if GPIO.input(4) == False:
+        os.system("omxplayer burp.wav")
+        time.sleep(1)
 
 
 
